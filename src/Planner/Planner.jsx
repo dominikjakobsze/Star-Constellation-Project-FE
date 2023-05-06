f8ca33a15c2ca1760aab19a4f883ad1b51e7f5eb
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-05-06 12:43:07 +0200

    Selectors base Fetch

--- src/Planner/Planner.jsx
+++ src/Planner/Planner.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Selector from "./SelectorContainer";
 
 const Planner = () => {
   return (<>
-    <h1>Planner</h1>
+    <div className="w-full max-w-screen flex flex-col flex-wrap items-center justify-center">
+        <div className="w-full max-w-[1920px] bg-gray-900 p-5 grid grid-cols-1">
+            <Selector/>
+        </div>
+    </div>
   </>)  
 };
 
---
+++ src/Planner/SelectorContainer.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+
+const SelectorContainer = () => {
+
+    const [stars, setStars] = React.useState([]);
+    const [constellations, setConstellations] = React.useState([]);
+
+    const fetchAllStars = async () => {
+        try {
+          const response = await fetch('http://localhost:3520/stars');
+          //console.log(response.status);
+          const data = await response.json();
+          setStars((prev) => data);
+            // console.log(data);
+            // console.log(data.status_code);
+            // console.log(data.error);
+            // console.log(data.stars_array);
+        } catch (error) {
+            // console.error(error);
+            // console.error(error.response);
+            // console.error(error.message);
+            // console.error(error.stack);
+        }
+      };
+      
+
+    React.useEffect(() => {
+        fetchAllStars();
+      }, []);
+
+    return (<>
+        <div onClick={() => console.log(stars)} className="w-full grid grid-cols-2 gap-5">
+            <div className="w-full flex flex-col flex-wrap gap-5">
+                <h1 className="w-full text-violet-400 font-bold text-xl text-center">Star Selector</h1>
+                <div className="w-full flex flex-col flex-wrap border-2 border-indigo-900 p-3 rounded-lg max-h-[400px]">
+                {stars.status_code !== 200 && stars.status_code !== 201 && !stars.error 
+                    ? (
+                        <p className="w-full text-center text-base font-medium text-indigo-900">Coś poszło nie tak</p>
+                    ) 
+                    : (stars.stars_array.length === 0
+                        ? (<p className="w-full text-center text-base font-medium text-indigo-900">0 Stars</p>)
+                        : "ok")}
+                </div>
+            </div>
+            <div className="w-full flex flex-col flex-wrap gap-5">
+                <h1 className="w-full text-violet-400 font-bold text-xl text-center">Star Selector</h1>
+                <div className="w-full flex flex-col flex-wrap border-2 border-indigo-900 p-3 rounded-lg max-h-[400px]">
+                {stars.status_code !== 200 && stars.status_code !== 201 && !stars.error 
+                    ? (
+                        <p className="w-full text-center text-base font-medium text-indigo-900">Coś poszło nie tak</p>
+                    ) 
+                    : (stars.stars_array.length === 0
+                        ? (<p className="w-full text-center text-base font-medium text-indigo-900">0 Stars</p>)
+                        : "ok")}
+                </div>
+            </div>
+        </div>
+    </>);
+};
+
+export default SelectorContainer;
