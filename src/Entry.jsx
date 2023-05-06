c470fd3c40b57bbe9ad68ec6902b38d9900823ca
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-05-06 11:48:27 +0200

    init planner

--- src/Entry.jsx
+++ src/Entry.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./reusable/NotFound/NotFound";
+import Planner from "./Planner/Planner";
 
 //define all you need here like route etc!
 //to match expected colors, you can use the following classes: violet-400, indigo-900, gray-900
@@ -13,6 +14,7 @@ const Entry = () => {
                 <Routes>
                     <Route path="/" element={"home"} />
                     <Route path="/about" element={"about"} />
+                    <Route path="/planner" element={<Planner/>} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
                 { /* //here you can define menu to make be available on all pages or any other components that are supposed to be on all pages */ }
---
+++ src/Planner/Planner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Planner = () => {
+  return (<>
+    <h1>Planner</h1>
+  </>)  
+};
+
+export default Planner;
