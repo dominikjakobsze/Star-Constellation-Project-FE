01035d8a32fb2f53c19e7bc210df87357361f6d1
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-05-08 13:42:47 +0200

    SkyOptions - fog, moon etc

--- src/Planner/Planner.jsx
+++ src/Planner/Planner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Selector from "./SelectorContainer";
+import SkyOptions from "./SkyOptions";
 
 const Planner = () => {
 
@@ -12,6 +13,7 @@ const Planner = () => {
     <p onClick={() => console.log(planner)}>click</p>
     <div className="w-full max-w-screen flex flex-col flex-wrap items-center justify-center">
         <div className="w-full max-w-[1920px] bg-gray-900 p-5 grid grid-cols-1">
+            <SkyOptions setPlanner={setPlanner}/>
             <Selector setPlanner={setPlanner}/>
         </div>
     </div>
---
+++ src/Planner/SkyOptions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//faza ksiezyca
+//opady atmosferyczne => jesli na niebie nie ma chmur to nie ma opadow
+//gestosc mgly
+//poziom zachmurzenia
+
+const SkyOptions = ({setPlanner}) => {
+    return (<>test</>);
+};
+
+export default SkyOptions;
