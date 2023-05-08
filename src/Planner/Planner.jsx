7baf1692c08d6909fd83282b950f03512f1ab2c6
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-05-08 13:33:31 +0200

    star & constellation selectors done

--- src/Planner/Planner.jsx
+++ src/Planner/Planner.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import Selector from "./SelectorContainer";
 
 const Planner = () => {
+
+  const [planner, setPlanner] = React.useState({
+    stars: [],
+    constellations: [],
+  });
+
   return (<>
+    <p onClick={() => console.log(planner)}>click</p>
     <div className="w-full max-w-screen flex flex-col flex-wrap items-center justify-center">
         <div className="w-full max-w-[1920px] bg-gray-900 p-5 grid grid-cols-1">
-            <Selector/>
+            <Selector setPlanner={setPlanner}/>
         </div>
     </div>
   </>)  
--- src/Planner/SelectorContainer.jsx
+++ src/Planner/SelectorContainer.jsx
@@ -2,39 +2,74 @@ import React from "react";
 import { IMAGE_BASE_URL } from "../constants";
 
 
-const SelectorContainer = () => {
+const SelectorContainer = ({setPlanner}) => {
 
     const [stars, setStars] = React.useState([]);
     const [constellations, setConstellations] = React.useState([]);
 
+
     const fetchAllStars = async () => {
         try {
           const response = await fetch('http://localhost:3520/stars');
-          //console.log(response.status);
           const data = await response.json();
           setStars((prev) => data);
-            // console.log(data);
-            // console.log(data.status_code);
-            // console.log(data.error);
-            // console.log(data.stars_array);
         } catch (error) {
-            // console.error(error);
-            // console.error(error.response);
-            // console.error(error.message);
-            // console.error(error.stack);
+            console.log(error);
         }
       };
-      
+
+    const fetchAllConstellations = async () => {
+        try {
+          const response = await fetch('http://localhost:3520/constellations');
+          const data = await response.json();
+          setConstellations((prev) => data);
+        } catch (error) {
+            console.log(error);
+        }
+      };
+    
+    const addSelectedStar = (event,star) => {
+        if(event.currentTarget.style.backgroundColor === "rgb(74, 0, 224)"){
+            event.currentTarget.style.backgroundColor = "rgb(17 24 39)";
+            setPlanner((prev) => ({
+                ...prev,
+                stars: prev.stars.filter((item) => item !== star.id)
+              }));
+        } else {
+            event.currentTarget.style.backgroundColor = "rgb(74, 0, 224)";
+            setPlanner((prev) => ({
+                ...prev,
+                stars: [...prev.stars, star.id]
+            }));
+        }
+    }
+
+    const addSelectedConstellation = (event,constellation) => {
+        if(event.currentTarget.style.backgroundColor === "rgb(74, 0, 224)"){
+            event.currentTarget.style.backgroundColor = "rgb(17 24 39)";
+            setPlanner((prev) => ({
+                ...prev,
+                constellations: prev.constellations.filter((item) => item !== constellation.id)
+              }));
+        } else {
+            event.currentTarget.style.backgroundColor = "rgb(74, 0, 224)";
+            setPlanner((prev) => ({
+                ...prev,
+                constellations: [...prev.constellations, constellation.id]
+            }));
+        }
+    }
 
     React.useEffect(() => {
         fetchAllStars();
+        fetchAllConstellations();
       }, []);
 
     return (<>
-        <div onClick={() => console.log(stars)} className="w-full grid grid-cols-2 gap-5">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5">
             <div className="w-full flex flex-col flex-wrap gap-5">
                 <h1 className="w-full text-violet-400 font-bold text-xl text-center">Star Selector</h1>
-                <div className="w-full flex flex-col flex-wrap border-2 border-indigo-900 p-3 rounded-lg max-h-[400px]">
+                <div className="w-full flex flex-col flex-wrap border-2 border-indigo-900 p-3 rounded-lg max-h-[400px] overflow-x-hidden overflow-y-auto">
                 {stars.status_code !== 200 && stars.status_code !== 201 && !stars.error 
                     ? (
                         <p className="w-full text-center text-base font-medium text-indigo-900">Coś poszło nie tak</p>
@@ -44,11 +79,12 @@ const SelectorContainer = () => {
                         : (
                             <div className="w-full grid grid-cols-1 gap-3">
                                 {stars.stars_array.map((star) => (
-                                    <div 
+                                    <div
+                                    onClick={(event) => addSelectedStar(event,star)} 
                                     key={star.id+star.name+star.linkToImage} 
-                                    className="w-full items-center justify-center flex flex-row flex-wrap gap-5 font-semibold text-violet-400">
+                                    className="w-full py-2 px-1 cursor-pointer hover:bg-indigo-900 items-center justify-center flex flex-row flex-nowrap gap-5 font-semibold text-violet-400">
                                         <p>{star.id}:</p>
-                                        <p>{star.name}</p>
+                                        <p className="truncate">{star.name}</p>
                                         <img src={IMAGE_BASE_URL+star.linkToImage} alt={star.name} className="w-10 h-10"/>
                                     </div>
                                 ))}
@@ -57,15 +93,28 @@ const SelectorContainer = () => {
                 </div>
             </div>
             <div className="w-full flex flex-col flex-wrap gap-5">
-                <h1 className="w-full text-violet-400 font-bold text-xl text-center">Star Selector</h1>
-                <div className="w-full flex flex-col flex-wrap border-2 border-indigo-900 p-3 rounded-lg max-h-[400px]">
-                {stars.status_code !== 200 && stars.status_code !== 201 && !stars.error 
+                <h1 className="w-full text-violet-400 font-bold text-xl text-center">Constellation Selector</h1>
+                <div className="w-full flex flex-col flex-wrap border-2 border-indigo-900 p-3 rounded-lg max-h-[400px] overflow-x-hidden overflow-y-auto">
+                {constellations.status_code !== 200 && constellations.status_code !== 201 && !constellations.error 
                     ? (
                         <p className="w-full text-center text-base font-medium text-indigo-900">Coś poszło nie tak</p>
                     ) 
-                    : (stars.stars_array.length === 0
-                        ? (<p className="w-full text-center text-base font-medium text-indigo-900">0 Stars</p>)
-                        : "ok")}
+                    : (constellations.constellations_array.length === 0
+                        ? (<p className="w-full text-center text-base font-medium text-indigo-900">0 Constellations</p>)
+                        : (
+                            <div className="w-full grid grid-cols-1 gap-3">
+                                {constellations.constellations_array.map((constellation) => (
+                                    <div
+                                    onClick={(event) => addSelectedConstellation(event,constellation)} 
+                                    key={constellation.id+constellation.name+constellation.linkToImage} 
+                                    className="w-full py-2 px-1 cursor-pointer hover:bg-indigo-900 items-center justify-center flex flex-row flex-nowrap gap-5 font-semibold text-violet-400">
+                                        <p>{constellation.id}:</p>
+                                        <p className="truncate">{constellation.name}</p>
+                                        <img src={IMAGE_BASE_URL+constellation.linkToImage} alt={constellation.name} className="w-10 h-10"/>
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
                 </div>
             </div>
         </div>
