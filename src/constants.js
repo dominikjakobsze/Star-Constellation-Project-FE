051d63d9fef689d2c063a1bb0acca2b4e636cc0e
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-05-09 12:08:45 +0200

    fixes

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const IMAGE_BASE_URL = "http://backend-js.devjakobsze.pl/uploads/";
-export const BASE_URL = "http://backend-js.devjakobsze.pl/";
+export const BASE_URL = "http://backend-js.devjakobsze.pl";
