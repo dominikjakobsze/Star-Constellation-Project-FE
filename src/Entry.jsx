ed2a42e8c05286c5ef3ff90daaa1cd580a15e6e5
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-05-16 17:57:01 +0200

    Planner Details

--- src/Entry.jsx
+++ src/Entry.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./reusable/NotFound/NotFound";
 import Planner from "./Planner/Planner";
+import PlannerBoard from "./Planner/PlannerBoard";
 
 //define all you need here like route etc!
 //to match expected colors, you can use the following classes: violet-400, indigo-900, gray-900
@@ -14,7 +15,9 @@ const Entry = () => {
                 <Routes>
                     <Route path="/" element={"home"} />
                     <Route path="/about" element={"about"} />
-                    <Route path="/planner" element={<Planner/>} />
+                    <Route path="/planner" element={<PlannerBoard/>} />
+                    <Route path="/planner/add/:date" element={<Planner/>} />
+                    <Route path="/planner/add/:date" element={<Planner/>} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
                 { /* //here you can define menu to make be available on all pages or any other components that are supposed to be on all pages */ }
---
+++ src/Planner/PlannerDetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PlannerDetails = () => {
+    return (<>
+        <div className="w-full max-w-screen flex flex-col flex-wrap items-start justify-start">
+            <div className="w-full max-w-[1920px] justify-start items-start bg-gray-900 min-h-screen p-5 flex flex-col flex-wrap gap-10">
+                test
+            </div>
+        </div>
+    </>);
+};
+
+export default PlannerDetails;
